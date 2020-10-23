e86e079e7f5945d9111de3cbe3808017782611b7
ymaa0063@student.monash.edu <64070414+1029YanMa@users.noreply.github.com>
2020-10-23 19:12:50 +1100

    fix one warning message

--- R/n_countries.R
+++ R/n_countries.R
@@ -1,11 +1,9 @@
 #' Count the number of countries
 #'
 #' @param data a data containing a country column
+#'
 #' @export
 #'
-"n_countries"
-
-
 n_countries <- function(data){
   dplyr::n_distinct(data$country)
 }
